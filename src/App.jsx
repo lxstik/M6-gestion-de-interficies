394e9a732e47f81d40b522ec77a84b8c53f4ca61
Yehor <156694343+lxstik@users.noreply.github.com>
2025-03-14 16:30:14 +0000

    logica login

--- src/App.jsx
+++ src/App.jsx
@@ -26,36 +26,35 @@ function App() {
             <div className="container-fluid">
               <a className="navbar-brand">Gestión de incidencias FPLLEFIA</a>
               <div>
-                <Link to="/Panel" className="btn btn-secondary ms-2">PANEL</Link>
-                <Link to="/InicioSesion" className="btn btn-secondary ms-2">LOGIN</Link>
-                <Link to="/Registro" className="btn btn-secondary ms-2">REGISTRO</Link>
+                {usuarioActual ? (
+                  <>
+                    <Link to="/Panel" className="btn btn-secondary ms-2">PANEL</Link>
+                  </>
+                ) : (
+                  <>
+                    <Link to="/InicioSesion" className="btn btn-secondary ms-2">LOGIN</Link>
+                    <Link to="/Registro" className="btn btn-secondary ms-2">REGISTRO</Link>
+                  </>
+                )}
               </div>
               <div>
-                {(() => {
-                  if (usuarioActual) {
-                    return (
-                      <div>
-                        <span>{usuarioActual}</span>
-                        <button onClick={cerrarSesion} className="btn btn-secondary ms-2">Cerrar Sesión</button>
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div>
-                        <span>Incógnito</span>
-                      </div>
-                    );
-                  }
-                })()}
+                {usuarioActual ? (
+                  <>
+                    <span>{usuarioActual}</span>
+                    <button onClick={cerrarSesion} className="btn btn-secondary ms-2">Cerrar Sesión</button>
+                  </>
+                ) : (
+                  <span>Incógnito</span>
+                )}
               </div>
             </div>
           </nav>
         </header>
         <div className="container mt-4" style={{ padding: 0 }}>
           <Routes>
-            <Route path="/Panel" element={<Panel />} />
-            <Route path="/InicioSesion" element={<InicioSesion setUsuarioActual={setUsuarioActual} />} />
-            <Route path="/Registro" element={<Registro />} />
+            <Route path="/Panel" element={usuarioActual ? <Panel /> : <InicioSesion setUsuarioActual={setUsuarioActual} />} />
+            <Route path="/InicioSesion" element={!usuarioActual ? <InicioSesion setUsuarioActual={setUsuarioActual} /> : <Panel />} />
+            <Route path="/Registro" element={!usuarioActual ? <Registro /> : <Panel />} />
           </Routes>
         </div>
       </Router>
